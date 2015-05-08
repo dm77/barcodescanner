ba8b5658ecc167130ee1e36a5890f33c07ad941c
rvdsoft <rvdsoft7@gmail.com>
2015-05-08 11:37:36 +0200

    Update DisplayUtils.java

--- core/src/main/java/me/dm7/barcodescanner/core/DisplayUtils.java
+++ core/src/main/java/me/dm7/barcodescanner/core/DisplayUtils.java
@@ -1,26 +1,29 @@
 package me.dm7.barcodescanner.core;
 
+
+import java.lang.reflect.Method;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Point;
+import android.os.Build;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.WindowManager;
 
+
 public class DisplayUtils {
-    public static Point getScreenResolution(Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        Point screenResolution = new Point();
-        if (android.os.Build.VERSION.SDK_INT >= 13) {
-            display.getSize(screenResolution);
-        } else {
-            screenResolution.set(display.getWidth(), display.getHeight());
-        }
-
-        return screenResolution;
+   
+	@SuppressLint("NewApi")
+	public static Point getScreenResolution(Context context) {
+    	 WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+         Display display = wm.getDefaultDisplay();
+         return getSize(display);
     }
-
-    public static int getScreenOrientation(Context context)
+  
+    @SuppressWarnings("deprecation")
+	public static int getScreenOrientation(Context context)
     {
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
@@ -37,5 +40,49 @@ public class DisplayUtils {
         }
         return orientation;
     }
+    //stackoverflow
+    public static Point getRealSize(Display display) {
+        Point outPoint = new Point();
+        Method mGetRawH;
+        try {
+            mGetRawH = Display.class.getMethod("getRawHeight");
+            Method mGetRawW = Display.class.getMethod("getRawWidth");
+            outPoint.x = (Integer) mGetRawW.invoke(display);
+            outPoint.y = (Integer) mGetRawH.invoke(display);
+            return outPoint;
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+
+    //stackoverflow
+    @SuppressWarnings("deprecation")
+	@SuppressLint("NewApi")
+	public static Point getSize(Display display) {
+        if (Build.VERSION.SDK_INT >= 17) {
+            Point outPoint = new Point();
+            DisplayMetrics metrics = new DisplayMetrics();
+            display.getRealMetrics(metrics);
+            outPoint.x = metrics.widthPixels;
+            outPoint.y = metrics.heightPixels;
+            return outPoint;
+        }
+        if (Build.VERSION.SDK_INT >= 14) {
+            Point outPoint = getRealSize(display);
+            if (outPoint != null)
+                return outPoint;
+        }
+        Point outPoint = new Point();
+        if (Build.VERSION.SDK_INT >= 13) {
+            display.getSize(outPoint);
+        } else {
+            outPoint.x = display.getWidth();
+            outPoint.y = display.getHeight();
+        }
+        return outPoint;
+    }
+    
+    
+   
 
 }
