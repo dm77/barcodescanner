9617ab8aa5b91dca6598a192092237f7a2b8b740
Thad Cox <T.Cox@cvent.com>
2016-04-26 10:20:58 -0400

    Fix Null Pointer Exception when camera is not acessable

--- core/src/main/java/me/dm7/barcodescanner/core/CameraWrapper.java
+++ core/src/main/java/me/dm7/barcodescanner/core/CameraWrapper.java
@@ -7,11 +7,19 @@ public class CameraWrapper {
     public final Camera camera;
     public final int cameraId;
 
-    public CameraWrapper(@NonNull Camera camera, int cameraId) {
+    private CameraWrapper(@NonNull Camera camera, int cameraId) {
         if (camera == null) {
             throw new NullPointerException("Camera cannot be null");
         }
         this.camera = camera;
         this.cameraId = cameraId;
+    }
+
+    public static CameraWrapper getWrapper(Camera camera, int cameraId) {
+        if (camera == null) {
+            return null;
+        } else {
+            return new CameraWrapper(camera, cameraId);
+        }
     }
 }
