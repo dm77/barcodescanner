24df32c098038395ef4d49981b8c203b16fa5dbc
Oscar Postlethwaite <oscar.postlethwaite@dyson.com>
2017-07-15 00:54:39 +0100

    fixed fragment activity

--- zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ShopScan.java
+++ zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ShopScan.java
@@ -1,9 +1,11 @@
 package me.dm7.barcodescanner.zbar.sample;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class ShopScan extends Activity {
+public class ShopScan extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
