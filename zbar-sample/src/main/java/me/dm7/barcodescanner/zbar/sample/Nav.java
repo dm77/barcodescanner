12e10610fef57e0d50c00292de4554e8bb7e02f7
TheObnoxiousFrog <jl15216@my.bristol.ac.uk>
2017-07-15 11:07:57 +0100

    merge

---
+++ zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/Nav.java
@@ -0,0 +1,14 @@
+package me.dm7.barcodescanner.zbar.sample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nav extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav);
+    }
+}
--- zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ShoppingList.java
+++ zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ShoppingList.java
@@ -1,7 +1,9 @@
 package me.dm7.barcodescanner.zbar.sample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ShoppingList extends Activity {
 
@@ -9,5 +11,14 @@ public class ShoppingList extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+        findViewById(R.id.findroute).setOnClickListener( new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent toNav = new Intent(ShoppingList.this, Nav.class);
+                 startActivity(toNav);
+             }
+        }
+        );
+
     }
 }
