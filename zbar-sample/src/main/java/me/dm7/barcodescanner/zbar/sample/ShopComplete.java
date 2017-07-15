76ea9c16adabb09f8cd2f95b119cbf59379e1112
Oscar Postlethwaite <oscar.postlethwaite@dyson.com>
2017-07-15 10:23:25 +0100

    Added shop complete

--- zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ShopComplete.java
+++ zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ShopComplete.java
@@ -1,7 +1,9 @@
 package me.dm7.barcodescanner.zbar.sample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShopComplete extends Activity {
 
@@ -9,5 +11,8 @@ public class ShopComplete extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_complete);
+        Intent intent = getIntent();
+        double totalPrice = intent.getDoubleExtra("price",0);
+        ((TextView)findViewById(R.id.completeprice)).setText("£" + totalPrice);
     }
 }
--- zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ShopScan.java
+++ zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ShopScan.java
@@ -2,18 +2,30 @@ package me.dm7.barcodescanner.zbar.sample;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ShopScan extends FragmentActivity implements ItemScannedListener {
 
     long lastPurchaseDate = 0;
+    double totalPrice = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_scan);
+        findViewById(R.id.donebutton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShopScan.this,ShopComplete.class);
+                intent.putExtra("price",totalPrice);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -26,7 +38,10 @@ public class ShopScan extends FragmentActivity implements ItemScannedListener {
                     ((EditText)findViewById(R.id.itemName)).setText(item.getName());
                     ((EditText)findViewById(R.id.price)).setText("£" + Double.toString(item.getPrice()));
                     ((EditText)findViewById(R.id.itemnum)).setText("2/34");
+                    totalPrice += item.getPrice();
+                    ((TextView)findViewById(R.id.totalPrice)).setText("Total Price: £" + totalPrice);
                 }
+
             }
             lastPurchaseDate = System.currentTimeMillis();
         }
