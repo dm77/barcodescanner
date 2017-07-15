1a5b5c6acaa8de952a12f249404f118a27067857
Oscar Postlethwaite <oscar.postlethwaite@dyson.com>
2017-07-15 09:34:54 +0100

    updated scanning activity

--- zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ItemList.java
+++ zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/ItemList.java
@@ -8,6 +8,6 @@ public class ItemList {
     public static ShopItem[] items = {
             new ShopItem("5018730000066", "Water", "£1"),
             new ShopItem("5449000011527", "Fanta", "50p"),
-            new ShopItem("544909000053879", "Coke", "50p")
+            new ShopItem("5449000053879", "Coke", "50p")
     };
 }
