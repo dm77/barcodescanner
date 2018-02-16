f46a2325272e89646c72cb60cae8d768df034129
Artur Sadowski <asadowski@3a.pl>
2018-02-16 18:02:54 +0100

    added exception catch to deal with getParameters()

--- core/src/main/java/me/dm7/barcodescanner/core/CameraUtils.java
+++ core/src/main/java/me/dm7/barcodescanner/core/CameraUtils.java
@@ -44,14 +44,18 @@ public class CameraUtils {
     public static boolean isFlashSupported(Camera camera) {
         /* Credits: Top answer at http://stackoverflow.com/a/19599365/868173 */
         if (camera != null) {
-            Camera.Parameters parameters = camera.getParameters();
+            try {
+                Camera.Parameters parameters = camera.getParameters();
 
-            if (parameters.getFlashMode() == null) {
-                return false;
-            }
+                if (parameters.getFlashMode() == null) {
+                    return false;
+                }
 
-            List<String> supportedFlashModes = parameters.getSupportedFlashModes();
-            if (supportedFlashModes == null || supportedFlashModes.isEmpty() || supportedFlashModes.size() == 1 && supportedFlashModes.get(0).equals(Camera.Parameters.FLASH_MODE_OFF)) {
+                List<String> supportedFlashModes = parameters.getSupportedFlashModes();
+                if (supportedFlashModes == null || supportedFlashModes.isEmpty() || supportedFlashModes.size() == 1 && supportedFlashModes.get(0).equals(Camera.Parameters.FLASH_MODE_OFF)) {
+                    return false;
+                }
+            } catch (Exception e) {
                 return false;
             }
         } else {
