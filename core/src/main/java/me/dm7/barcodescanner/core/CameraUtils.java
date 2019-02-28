66e83d894cc781b09c24bfbe9fc06d26c74442f9
Hamidreza Etebarian <shetmobile@gmail.com>
2019-02-28 15:53:44 +0330

    CameraUtils.java try/catch in support flash issue #235 & #416

--- core/src/main/java/me/dm7/barcodescanner/core/CameraUtils.java
+++ core/src/main/java/me/dm7/barcodescanner/core/CameraUtils.java
@@ -5,12 +5,16 @@ import android.hardware.Camera;
 import java.util.List;
 
 public class CameraUtils {
-    /** A safe way to get an instance of the Camera object. */
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
     public static Camera getCameraInstance() {
         return getCameraInstance(getDefaultCameraId());
     }
 
-    /** Favor back-facing camera by default. If none exists, fallback to whatever camera is available **/
+    /**
+     * Favor back-facing camera by default. If none exists, fallback to whatever camera is available
+     **/
     public static int getDefaultCameraId() {
         int numberOfCameras = Camera.getNumberOfCameras();
         Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
@@ -25,17 +29,18 @@ public class CameraUtils {
         return defaultCameraId;
     }
 
-    /** A safe way to get an instance of the Camera object. */
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
     public static Camera getCameraInstance(int cameraId) {
         Camera c = null;
         try {
-            if(cameraId == -1) {
+            if (cameraId == -1) {
                 c = Camera.open(); // attempt to get a Camera instance
             } else {
                 c = Camera.open(cameraId); // attempt to get a Camera instance
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
@@ -43,18 +48,24 @@ public class CameraUtils {
 
     public static boolean isFlashSupported(Camera camera) {
         /* Credits: Top answer at http://stackoverflow.com/a/19599365/868173 */
-        if (camera != null) {
-            Camera.Parameters parameters = camera.getParameters();
+        try {
+            if (camera != null) {
+                Camera.Parameters parameters = camera.getParameters();
 
-            if (parameters.getFlashMode() == null) {
+                if (parameters.getFlashMode() == null) {
+                    return false;
+                }
+
+                List<String> supportedFlashModes = parameters.getSupportedFlashModes();
+                if (supportedFlashModes == null || supportedFlashModes.isEmpty() || supportedFlashModes.size() == 1 && supportedFlashModes.get(0).equals(Camera.Parameters.FLASH_MODE_OFF)) {
+                    return false;
+                }
+            } else {
                 return false;
             }
 
-            List<String> supportedFlashModes = parameters.getSupportedFlashModes();
-            if (supportedFlashModes == null || supportedFlashModes.isEmpty() || supportedFlashModes.size() == 1 && supportedFlashModes.get(0).equals(Camera.Parameters.FLASH_MODE_OFF)) {
-                return false;
-            }
-        } else {
+        } catch (Exception e) {
+            e.printStackTrace();
             return false;
         }
 
